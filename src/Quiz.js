4083035307e62095fbea6f2e014025e2bab19249
Morgan Jones <jonesml2604@gmail.com>
2018-10-28 14:54:24 +0000

    Created QuizEnd that displays on condition

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import QuizQuestion from './QuizQuestion';
+import QuizEnd from './QuizEnd';
 
 let quizData = require('./quiz_data.json');
 
@@ -9,12 +10,21 @@ class Quiz extends Component{
         this.state = {quiz_position: 1}
     }
 
+
     render(){
+        
+        const isQuizEnd = ((this.state.quiz_position -1) == quizData.quiz_questions.length);
+
         return(
             // Quiz Question Component takes a prop called quiz_question.
             // The prop refrences the JSON data via the variable quizData and refrences the quiz_question JSON array.
             // The quiz_position state is used to access the question (-1 since array start at 0)
-            <QuizQuestion quiz_question= {quizData.quiz_questions[this.state.quiz_position -1]}/>
+
+            // JSX Conditional used to display the component if isQuizEnd = true
+            <div>
+                {isQuizEnd ? <QuizEnd /> :
+                <QuizQuestion quiz_question= {quizData.quiz_questions[this.state.quiz_position -1]}/>}
+            </div>
         );
     }
 
